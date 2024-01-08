2af2edb32026e3918ae88c0fa9d8ad66aeb1bebf
Vit0rrr <vitorhmeira@gmail.com>
2024-01-08 10:41:49 -0300

    app de orçamento

---
+++ app.js
@@ -0,0 +1,72 @@
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+
+	validardados(){
+		for ( let  i in this){
+			if ( this[i] == undefined || this[i] == '' || this[i] == null ){
+				return false
+			}
+		}
+		return true
+	}
+}
+
+class Bd {
+
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
+
+function cadastrarDespesa() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+
+	if(despesa.validardados()){
+		bd.gravar(despesa)
+		$('#sucessogravacao').modal(`show`)
+	}else{
+		$('#errogravacao').modal(`show`)
+	}
+}
